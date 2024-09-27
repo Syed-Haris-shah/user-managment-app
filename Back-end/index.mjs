886a8bfe2432f1ba165b1ee0541f97d6a513780c
Syed Haris shah <haris.official222@gmail.com>
2024-09-27 20:37:07 +0500

    updated index file and add post method

--- Back-end/index.mjs
+++ Back-end/index.mjs
@@ -1,12 +1,23 @@
 import express from 'express'
+import cors from 'cors';
 
 const PORT = 3000;
 
 const app = express();
+app.use(cors());
+app.use(express.json());
 
-app.get('/',(req, res)=>{
-  res.json({message: 'heloo'})
+const users = [];
+
+app.get('/users',(req, res)=>{
+  res.json(users);
 })
+
+app.post('/users', (req, res) => {
+  const newUser = { id: Date.now(), task: req.body.task };
+  users.push(newUser);
+  res.json(newUser);
+});
 
 app.listen(PORT,()=>{
   console.log(`port in running on localhost:${PORT}`)
