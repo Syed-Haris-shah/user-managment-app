68435cb9acfe3996b2191bcd3c0f32456f575dd1
Syed Haris shah <haris.official222@gmail.com>
2024-09-27 20:32:03 +0500

    created server successfully

--- Back-end/index.mjs
+++ Back-end/index.mjs
@@ -1,1 +1,13 @@
-console.log('hello')
+import express from 'express'
+
+const PORT = 3000;
+
+const app = express();
+
+app.get('/',(req, res)=>{
+  res.json({message: 'heloo'})
+})
+
+app.listen(PORT,()=>{
+  console.log(`port in running on localhost:${PORT}`)
+})
